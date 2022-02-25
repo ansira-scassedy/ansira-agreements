c70e06bfc3635d735729f5307dad6ca6cded45d4
Shawn Cassedy <shawn.cassedy@ansira.com>
2022-02-25 12:06:45 -0600

    adding amplify with auth

---
+++ src/agreements-admin/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ansiraagreementssrcagreementsadmin": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
