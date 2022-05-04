9273732431ad5c12d693fc557c5130b76fe21fbf
Shawn Cassedy <shawn.cassedy@ansira.com>
2022-05-04 12:41:40 -0500

    updated backend details

--- src/amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ src/amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,21 +5,6 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         },
-        "AgreementsDocument": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        },
-        "Documents": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        },
-        "Agreements": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        },
         "AnsiraAgreementAPI": {
             "RootUrl": "string",
             "ApiName": "string",
@@ -27,24 +12,6 @@ export type AmplifyDependentResourcesAttributes = {
         }
     },
     "function": {
-        "documentsAPI": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "APIDocuments": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "APIAgreements": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
         "APIAnsiraAgreements": {
             "Name": "string",
             "Arn": "string",
