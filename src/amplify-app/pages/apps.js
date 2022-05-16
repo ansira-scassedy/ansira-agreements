e2ce0cade65d284d0fbea8ed89311e890c9dbc95
Shawn Cassedy <shawn.cassedy@ansira.com>
2022-05-16 16:38:10 -0500

    updated layout and auth

--- src/amplify-app/pages/apps.js
+++ src/amplify-app/pages/apps.js
@@ -1,230 +1,61 @@
 import Head from "next/head";
-import { useState, useEffect} from "react";
-import { useAuthenticator, Authenticator } from '@aws-amplify/ui-react';
-
-
-import {
-  ScaleIcon,
-} from "@heroicons/react/outline";
-import {
-  CashIcon,
-  ChevronRightIcon,
-} from "@heroicons/react/solid";
-
-import AdminLayout from "../components/layout/admin";
-
-//components
-import EditSlideOver from "../components/appRegistry/edit_slideover";
+import { useState, useEffect } from "react";
+import Layout from "../components/layout/layout";
 
 //amplify stuff
-import { DataStore } from '@aws-amplify/datastore';
-import { AppRegistry } from '../models';
+import { DataStore } from "@aws-amplify/datastore";
+import { AppRegistry } from "../models";
 //
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Apps() {
   const [apps, setapps] = useState([]);
   const [open, setOpen] = useState(true);
   // const { user, signOut } = useAuthenticator((context) => [context.user]);
 
-    useEffect(() => {
+  useEffect(() => {
     fetchApps();
     async function fetchApps() {
       //todo check user role and get email or id to verify createdby
-      const appData = await DataStore.query(AppRegistry, a => a.createdBy("eq", "shawn.cassedy@ansira.com"));
+      const appData = await DataStore.query(AppRegistry, (a) =>
+        a.createdBy("eq", "shawn.cassedy@ansira.com")
+      );
       setapps(appData);
     }
 
     // const subscription = DataStore.observe(AppRegistry).subscribe(() => fetchApps())
     // return () => subscription.unsubscribe()
-
-
   }, []);
 
   return (
     <>
-    {/* <EditSlideOver></EditSlideOver> */}
-    <AdminLayout>
-      {/* Page header */}
-      <div className="bg-white shadow">
-  
-        <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-   
-          <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
-            <div className="flex-1 min-w-0">
-              {/* Profile */}
-            </div>
-            <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4">
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-              >
-                Register App
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-8">
-        <h2 className="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
-          Registered Applications
-        </h2>
-        {/* Activity list (smallest breakpoint only) */}
-        <div className="shadow sm:hidden">
-          <ul
-            role="list"
-            className="mt-2 divide-y divide-gray-200 overflow-hidden shadow sm:hidden"
-          >
-            {apps.map((transaction) => (
-              <li key={transaction.id}>
-                <a
-                  href={transaction.href}
-                  className="block px-4 py-4 bg-white hover:bg-gray-50"
-                >
-                  <span className="flex items-center space-x-4">
-                    <span className="flex-1 flex space-x-2 truncate">
-                      <CashIcon
-                        className="flex-shrink-0 h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                      <span className="flex flex-col text-gray-500 text-sm truncate">
-                        <span className="truncate">{transaction.name}</span>
-                        <span>
-                          <span className="text-gray-900 font-medium">
-                            {transaction.amount}
-                          </span>{" "}
-                          {transaction.currency}
-                        </span>
-                        <time dateTime={transaction.date}>
-                          {transaction.date}
-                        </time>
-                      </span>
-                    </span>
-                    <ChevronRightIcon
-                      className="flex-shrink-0 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </a>
-              </li>
-            ))}
-          </ul>
-
-          <nav
-            className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200"
-            aria-label="Pagination"
-          >
-            <div className="flex-1 flex justify-between">
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-              >
-                Previous
-              </a>
-              <a
-                href="#"
-                className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-              >
-                Next
-              </a>
-            </div>
-          </nav>
-        </div>
-
-        {/* Activity table (small breakpoint and up) */}
-        <div className="hidden sm:block">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col mt-2">
-              <div className="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead>
-                    <tr>
-                      <th className="px-6 py-2 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        App Name
-                      </th>
-                      <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Description
-                      </th>
-                      <th className="hidden px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider md:block">
-                        Status
-                      </th>
-                      {/* <th className="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Date
-                      </th> */}
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {apps.map((app) => (
-                      <tr key={app.id} className="bg-white">
-                        <td className="max-w-0 w-full px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                          <div className="flex">
-                            <a
-                              href="#"
-                              className="group inline-flex space-x-2 truncate text-sm"
-                            >
-                              <CashIcon
-                                className="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                                aria-hidden="true"
-                              />
-                              <p className="text-gray-500 truncate group-hover:text-gray-900">
-                                {app.name}
-                              </p>
-                            </a>
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
-                          <span className="text-gray-900 font-medium">
-                            {app.description}{" "}
-                          </span>
-                        </td>
-                        <td className="hidden px-6 py-4 whitespace-nowrap text-sm text-gray-500 md:block">
-                        
-                        <span className={app.isActive ? 'inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800': 'inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800'}>
-                        {app.isActive ? 'Active': 'Inactive'}
-                        </span>
-  
-                        </td>
-                        {/* <td className="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                          <time dateTime={app.createdAt}>{app.createdAt}</time>
-                        </td> */}
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                {/* Pagination */}
-                <nav
-                  className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
-                  aria-label="Pagination"
-                >
-                  <div className="hidden sm:block">
-                    <p className="text-sm text-gray-700">
-                      Showing <span className="font-medium">1</span> to{" "}
-                      <span className="font-medium">10</span> of{" "}
-                      <span className="font-medium">20</span> results
-                    </p>
+      {/* <EditSlideOver></EditSlideOver> */}
+      <Layout>
+        {/* Page header */}
+        <div className="bg-white shadow">
+          <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+              <div className="flex-1 min-w-0">
+                {/* Profile */}
+                <div className="flex items-center">
+                  <div>
+                    <div className="flex items-center">
+                      <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                        App Registry
+                      </h1>
+                    </div>
                   </div>
-                  <div className="flex-1 flex justify-between sm:justify-end">
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                    >
-                      Previous
-                    </a>
-                    <a
-                      href="#"
-                      className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                    >
-                      Next
-                    </a>
-                  </div>
-                </nav>
+                </div>
               </div>
+              <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4"></div>
             </div>
           </div>
         </div>
-      </div>
-    </AdminLayout>
+        <div className="mt-8"></div>
+      </Layout>
     </>
   );
 }
--- src/amplify-app/pages/index.js
+++ src/amplify-app/pages/index.js
@@ -1,35 +1,44 @@
-import Head from 'next/head'
-import { useState, useEffect } from 'react'
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import Layout from "../components/layout/layout";
 
-import AdminLayout from '../components/layout/admin'
-
+//amplify stuff
+import { DataStore } from "@aws-amplify/datastore";
+import { AppRegistry } from "../models";
+//
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
-
-
 
 export default function Home() {
 
   return (
-
-    <AdminLayout>
-      <main>
-        <div className="py-6">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-          </div>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            {/* Replace with your content */}
-            <div className="py-4">
-              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+    <>
+      {/* <EditSlideOver></EditSlideOver> */}
+      <Layout>
+        {/* Page header */}
+        <div className="bg-white shadow">
+          <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+              <div className="flex-1 min-w-0">
+                {/* Profile */}
+                <div className="flex items-center">
+                  <div>
+                    <div className="flex items-center">
+                      <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                        Dashboard
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4"></div>
             </div>
-            {/* /End replace */}
           </div>
         </div>
-      </main>
-    </AdminLayout>
-
-  )
+        <div className="mt-8"></div>
+      </Layout>
+    </>
+  );
 }
